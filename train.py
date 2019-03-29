17014170e2e626ee6f7253dbc0d491192c0b5674
Sumit Das <52028512@acsind.com>
2019-03-29 06:39:14 +0530

    epoch count reduced

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
-# epochs = 1
-epochs = 50
+epochs = 1
+# epochs = 50
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
